3deffffbed5c4d56c91ca3cc86bb3a6d818bf872
Vinu K <kevy.vinu@gmail.com>
2021-09-13 11:29:53 +0530

    modified files

--- rhel8/hosts.sh
+++ rhel8/hosts.sh
@@ -1,3 +1,6 @@
+# Creates Ansible inventory file
+
+function CREATE_INVENTORY {
 cat << EOF > hosts
 [all:vars]
 ansible_user=user
@@ -7,5 +10,8 @@ ansible_ssh_private_key_file=$(dirname $(pwd))/downloads/id_ed25519
 openshift_kubeconfig_path=$(dirname $(pwd))/cluster-files/auth/kubeconfig
 
 [new_workers] 
-192.168.122.198
+192.168.122.98
 EOF
+}
+
+CREATE_INVENTORY
