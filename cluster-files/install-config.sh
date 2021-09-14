090a35751f6711d18d73bfcd1ca5967cee054fea
Vinu K <kevy.vinu@gmail.com>
2021-09-14 22:12:21 +0530

    added modification

--- cluster-files/install-config.sh
+++ cluster-files/install-config.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # Populate install-config.yaml file using the ENV
 
+source $(dirname $(pwd))/env
+
 function SSH_CHECK {
 	if ! [[ -f $(dirname $(pwd))/downloads/id_ed25519.pub ]]; then
 		echo "SSH public key is not present in the downloads directory. Please run sshAndPullsecret.sh"
 		exit 1
+        else
+                SSHKEY=$(cat $(dirname $(pwd))/downloads/id_ed25519.pub)
 	fi
 }
 
@@ -12,10 +16,12 @@ function PULL_SECRET_CHECK {
 	if ! [[ -f $(dirname $(pwd))/downloads/pull-secret.json ]]; then
 		echo "Pull secret is not present in the downloads directory. Please run sshAndPullsecret.sh"
 		exit 1
+        else
+                PULLSECRET=$(cat $(dirname $(pwd))/downloads/pull-secret.json)
 	fi
 }
 
-function POPULATE_INSTALL_CONFIG {
+function DISCONNECTED {
 cat << EOF > install-config.yaml
 apiVersion: v1
 baseDomain: ${DOMAIN}
@@ -49,11 +55,118 @@ EOF
 cp install-config.yaml install-config.yaml.bkp
 }
 
-SSH_CHECK
-PULL_SECRET_CHECK
+function CONNECTED_PROXY {
+cat << EOF > install-config.yaml
+apiVersion: v1
+baseDomain: ${DOMAIN}
+compute: 
+- hyperthreading: Enabled 
+  name: worker
+  replicas: 2 
+controlPlane: 
+  hyperthreading: Enabled 
+  name: master
+  replicas: 3 
+metadata:
+  name: ${CLUSTER_NAME}
+networking:
+  clusterNetwork:
+  - cidr: 10.128.0.0/14 
+    hostPrefix: 23 
+  networkType: OpenShiftSDN
+  serviceNetwork: 
+  - 172.30.0.0/16
+platform:
+  none: {} 
+fips: false 
+pullSecret: '${PULLSECRET}' 
+sshKey: '${SSHKEY}'
+proxy:
+  httpProxy: http://192.168.122.1:31128
+  httpsProxy: https://192.168.122.1:31128
+  noProxy: example.local
+EOF
+cp install-config.yaml install-config.yaml.bkp
+}
 
-source $(dirname $(pwd))/env
-PULLSECRET=$(cat $(dirname $(pwd))/downloads/pull-secret.json)
-SSHKEY=$(cat $(dirname $(pwd))/downloads/id_ed25519.pub)
+function CONNECTED_NON_PROXY {
+cat << EOF > install-config.yaml
+apiVersion: v1
+baseDomain: ${DOMAIN}
+compute: 
+- hyperthreading: Enabled 
+  name: worker
+  replicas: 2 
+controlPlane: 
+  hyperthreading: Enabled 
+  name: master
+  replicas: 3 
+metadata:
+  name: ${CLUSTER_NAME}
+networking:
+  clusterNetwork:
+  - cidr: 10.128.0.0/14 
+    hostPrefix: 23 
+  networkType: OpenShiftSDN
+  serviceNetwork: 
+  - 172.30.0.0/16
+platform:
+  none: {} 
+fips: false 
+pullSecret: '${PULLSECRET}' 
+sshKey: '${SSHKEY}'
+EOF
+cp install-config.yaml install-config.yaml.bkp
+}
 
-POPULATE_INSTALL_CONFIG
+function USAGE {
+	echo "Usage: bash $0 -t <connnected|disconnected> -i <proxy|non-proxy>" 1>&2; exit 1;
+}
+
+function VALIDATE {
+	echo "Why disconnected + proxy ?!?" 1>&2; exit 1;
+}
+
+while getopts ":t:i:" o; do
+    case "${o}" in
+        t)
+            t=${OPTARG}
+            ;;
+        i)
+            i=${OPTARG}
+	    if [[ "${t}" == "disconnected" ]]; then
+		    if [[ "${i}" == "proxy" ]]; then
+			    VALIDATE
+		    elif [[ "${i}" == "non-proxy" ]]; then
+                            SSH_CHECK
+                            PULL_SECRET_CHECK
+			    DISCONNECTED
+		    else
+			    USAGE
+		    fi
+	    elif [[ "${t}" == "connected" ]]; then
+		    if [[ "${i}" == "proxy" ]]; then
+                            SSH_CHECK
+                            PULL_SECRET_CHECK
+			    CONNECTED_PROXY
+		    elif [[ "${i}" == "non-proxy" ]]; then
+                            SSH_CHECK
+                            PULL_SECRET_CHECK
+			    CONNECTED_NON_PROXY
+		    else 
+			    USAGE
+		    fi
+	    else
+		    USAGE
+	    fi
+            ;;
+        *)
+            USAGE
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${t}" ] || [ -z "${i}" ]; then
+    USAGE
+fi
