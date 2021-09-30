cc294f9c9a8c6af04c7d1d6c08ce398f240d8b1e
Vinu K <kevy.vinu@gmail.com>
2021-09-30 11:54:50 +0530

    Added condition for checking checksum.yaml creation

--- registry/mirror.sh
+++ registry/mirror.sh
@@ -20,7 +20,9 @@ DIGEST="$(oc adm release info quay.io/openshift-release-dev/ocp-release:${OCP_RE
 DIGEST_ALGO="${DIGEST%%:*}"
 DIGEST_ENCODED="${DIGEST#*:}"
 SIGNATURE_BASE64=$(curl -s "https://mirror.openshift.com/pub/openshift-v4/signatures/openshift/release/${DIGEST_ALGO}=${DIGEST_ENCODED}/signature-1" | base64 -w0 && echo)
-[[ -z $SIGNATURE_BASE64 ]] || echo "✗ ConfigMap creation failed"
+if [ -z ${SIGNATURE_BASE64} ]; then
+	echo "✗ ConfigMap creation failed"
+fi
 cat >checksum-${OCP_RELEASE_NUMBER}.yaml <<EOF
 apiVersion: v1
 kind: ConfigMap
